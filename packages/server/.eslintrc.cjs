fe9f196e884e81eab0985aab12701ceef246b79b
OzhoginCode <danozh.b@gmail.com>
2024-11-23 18:53:27 +0200

    setup basic project structure

---
+++ packages/server/.eslintrc.cjs
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: false,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/strict-type-checked',
+    // 'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
+  ignorePatterns: ['.eslintrc.cjs'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    projectService: true,
+    project: './tsconfig.app.json',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+    react: { version: '18.3' },
+  },
+  
+  plugins: ['react-refresh', '@typescript-eslint'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/prop-types': 'off',
+    'import/extensions': 'off',
+    'object-curly-newline': ['error', {
+      'minProperties': 5
+    }],
+    'react/function-component-definition': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/jsx-filename-extension': [0,
+        {
+          "extensions": [
+            ".tsx"
+          ]
+        }
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-confusing-void-expression': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/unbound-method': 'off'
+  },
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+  ]
+}
